7210713997707185314c73c73e4c3da36b7dd9a7
simple-starters-project <128161944+simple-starters-project@users.noreply.github.com>
2024-03-26 13:36:22 +0100

    php: added template for forms exercice

---
+++ php_examples/cours_php_4_formulaires /php/nav.php
@@ -0,0 +1,12 @@
+<!-- Ici, nous définissons l'espace réservé à la navigation -->
+<!-- https://developer.mozilla.org/fr/docs/Web/HTML/Element/nav -->
+<nav>
+    <!-- Ici, nous avons 3 ancres qui permettent la navigation entre les pages -->
+    <!-- https://developer.mozilla.org/fr/docs/Web/HTML/Element/a -->
+    <!-- Navigation vers la page Accueil-->
+    <a href="index.php">Accueil</a>
+    <!-- Navigation vers la page Forum -->
+    <a href="forum.php">Forum</a>
+    <!-- Navigation vers la page Contact -->
+    <a href="contact.php">Contact</a>
+</nav>
---
+++ php_examples/cours_php_4_formulaires /result.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Cela définit l'encodage des caractères de notre page -->
+    <meta charset="UTF-8">
+    <!-- Il s'agit du nom de l'onglet -->
+    <title>My Blog - Contact</title>
+    <!-- Ceci nous permet de faire le lien avec notre fichier css -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<!-- J'utilise le PHP afin de factoriser mon code. Ici, j'importe le header sur mes pages afin que le code du header n'existe qu'à un seul endroit -->
+<!-- Documentation : https://www.php.net/manual/en/function.require-once.php -->
+<?php require_once('php/header.php') ?>
+
+<!-- J'utilise le PHP afin de factoriser mon code. Ici, j'importe la navigation sur mes pages afin que le code du header n'existe qu'à un seul endroit -->
+<!-- Documentation : https://www.php.net/manual/en/function.require-once.php -->
+<?php require_once('php/nav.php') ?>
+
+<main>
+
+<?php 
+
+// Ecrivez votre code de résultat ici 
+
+
+?>
+
+</main>
+
+<!-- J'utilise le PHP afin de factoriser mon code, ici j'importe le footer sur mes pages afin que le code du footer n'existe qu'à un seul endroit -->
+<!-- Documentation : https://www.php.net/manual/en/function.require-once.php -->
+<?php require_once('php/footer.php') ?>
+
+</body>
+
+</html>
