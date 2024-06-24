694a171022a73fa165a5b9659eb3c22970b3a93a
simple-starters-project <ilies.lebcir.pro@gmail.com>
2024-06-24 09:37:00 +0200

    :sparkles: tp7 template update

--- php_examples/cours_php_7_connexion/php/nav.php
+++ php_examples/cours_php_7_connexion/php/nav.php
@@ -10,25 +10,16 @@
 
     <?php
 
-    /* Ici on utilise "session_start()" afin d'initialiser une session */
-    /* Cette instruction est indispensable pour pouvoir lire ou écrire dans la variable $_SESSION */
-    /* Comme la nav est présente sur toutes les pages, je n'ai pas besoin d'utiliser "session_start()" ailleurs */
-    /* https://www.php.net/manual/fr/function.session-start.php */
-    session_start();
 
+    /* TODO : Mettez en place un bouton de déconnexion */
 
-    /* Ici je vérifie l'état de l'utilisateur et affiche une navigation différente en fonction de son état */
-    if (isset($_SESSION["email"]) && !empty($_SESSION["email"])) {
-        /* Navigation vers la page Utilisateurs, affichée uniquement si l'utilisateur est connecté */
-        echo "<a href=\"users.php\">Utilisateurs</a>";
-        /* Navigation vers la page Logout, affichée uniquement si l'utilisateur est connecté */
-        echo "<a href=\"logout.php\">Déconnexion</a>";
-    } else {
-        /* Navigation vers la page Inscription */
-        echo "<a href=\"register.php\">Inscription</a>";
-        /* Navigation vers la page Connexion, affichée uniquement si l'utilisateur n'est pas connecté */
-        echo "<a href=\"login.php\">Connexion</a>";
-    }
+    /* Navigation vers la page Utilisateurs, affichée uniquement si l'utilisateur est connecté */
+    echo "<a href=\"users.php\">Utilisateurs</a>";
+    /* Navigation vers la page Inscription */
+    echo "<a href=\"register.php\">Inscription</a>";
+    /* Navigation vers la page Connexion, affichée uniquement si l'utilisateur n'est pas connecté */
+    echo "<a href=\"login.php\">Connexion</a>";
+
 
     ?>
 
